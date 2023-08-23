b2e417ddd2f0b17c7c9fc31cadeeb3fb2b47583c
“seetha1730” <seetharaj1990@gmail.com>
2023-08-23 19:02:38 +0200

    lab express spotify

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
+const port =3000;
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
@@ -12,7 +14,64 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
-
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  spotifyApi
+  .clientCredentialsGrant()
+  .then(data =>{
+    spotifyApi.setAccessToken(data.body['access_token'])
+  })
+  .catch(error => console.log('Something went wrong when retrieving an access token', error));
 // Our routes go here:
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+// 
+
+
+app.get("/", (req, res) => {
+  res.render("home")
+});
+app.get("/artist-search", (req, res) => {
+  const { artistSearch } = req.query
+  
+
+
+  spotifyApi
+.searchArtists(artistSearch)
+.then(data => {
+  console.log('The received data from the API: ', data.body);
+  // ----> 'HERE'S WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+  const albums= data.body.artists.items
+  console.log(albums)
+  res.render("artist-search",{albums:albums})
+ 
+})
+.catch(err => console.log('The error while searching artists occurred: ', err));
+
+ 
+});
+
+
+app.get('/albums/:id', (req, res) => {
+  const  artistSearchid  = req.params.id
+
+  spotifyApi.getArtistAlbums(artistSearchid).then(data =>{
+   
+    const viewAlbum = data.body.items   
+  res.render("albums",{viewAlbum:viewAlbum})
+   })
+});
+
+app.get('/track/:id', (req, res) => {
+  const albumId  = req.params.id
+
+  spotifyApi.getAlbumTracks(albumId).then(data =>{
+   
+    const viewtracks = data.body.items
+
+  res.render("track",{viewtracks})
+   })
+});
+app.listen(port, () => console.log(`My Spotify project running on port ${port} 🎧 🥁 🎸 🔊`));
